e546260e091b17dba3227fb27f6cd3c4d4300bc0
paulooliveira152012 <paulooliveira152012@gmail.com>
2025-02-26 11:54:28 -0500

    before canva hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import "../styles/style.css";
 import "../styles/hero.css";
 import { useNavigate } from "react-router-dom";
-import heroImg from "../assets/images/heronovaimage.jpg"
+import heroImg from "../assets/images/heronovaimage.jpg";
 import Header from "./Header";
+import Elipse from "../assets/images/Ellipse.svg"
 // import video from "../assets/videos/heroVideo.MP4";
 
 const Hero = () => {
@@ -11,11 +12,11 @@ const Hero = () => {
     // <div className="hero" role="banner" aria-labelledby="motor picture">
     //   <div className="darkHeroOverlay" aria-hidden="true"></div>
     //   <div className="heroContent">
-    //     <video 
-    //       autoPlay 
-    //       loop 
-    //       muted 
-    //       playsinline 
+    //     <video
+    //       autoPlay
+    //       loop
+    //       muted
+    //       playsinline
     //       class="backgroundVideo"
     //       aria-hidden="true"
     //       >
@@ -25,20 +26,19 @@ const Hero = () => {
     //   </div>
     // </div>
 
-
     <div className="hero" role="banner" aria-labelledby="motor picture">
-  <Header className="blackHeader transparent"/>
-   <img className="heroImg" src={heroImg} />
+      <Header className="blackHeader transparent" />
+      <img className="heroImg" />
       <div className="darkHeroOverlay" aria-hidden="true"></div>
       <div className="heroContent">
-         <h1>
+        <h1>
           Your vision, <br />
           our inovation
         </h1>
         <button onClick={() => navigate("/contact-us")}>Contact us</button>
+        <img src={Elipse} className="ellipse" />
       </div>
     </div>
-
   );
 };
 
